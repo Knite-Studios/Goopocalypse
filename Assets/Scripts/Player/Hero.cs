44cd8ff4fa20a7b8b9736a11ace984f0d3bf05b0
KingRainbow44 <kobedo11@gmail.com>
2024-05-15 12:20:12 -0400

    chore(attributes): Switch back to trait-oriented attribute system

--- Assets/Scripts/Player/Hero.cs
+++ Assets/Scripts/Player/Hero.cs
@@ -1,26 +1,27 @@
-﻿using Systems.Attributes;
+﻿using System.Collections.Generic;
+using Systems.Attributes;
 using XLua;
 
 namespace Player
 {
     [LuaCallCSharp]
-    public class Hero : Attributable
+    public class Hero : IAttributable
     {
         public string Name { get; set; }
-        public int Health => GetAttributeValue<int>(Attribute.Health);
-        public float Stamina => GetAttributeValue<float>(Attribute.Stamina);
-        public float Speed => GetAttributeValue<float>(Attribute.Speed);
-        public float AttackSpeed => GetAttributeValue<float>(Attribute.AttackSpeed);
-        public int AttackDamage => GetAttributeValue<int>(Attribute.AttackDamage);
-        public int Armor => GetAttributeValue<int>(Attribute.Armor);
-        public float AreaOfEffect => GetAttributeValue<float>(Attribute.AreaOfEffect);
+        public int Health => this.GetAttributeValue<int>(Attribute.Health);
+        public float Stamina => this.GetAttributeValue<float>(Attribute.Stamina);
+        public float Speed => this.GetAttributeValue<float>(Attribute.Speed);
+        public float AttackSpeed => this.GetAttributeValue<float>(Attribute.AttackSpeed);
+        public int AttackDamage => this.GetAttributeValue<int>(Attribute.AttackDamage);
+        public int Armor => this.GetAttributeValue<int>(Attribute.Armor);
+        public float AreaOfEffect => this.GetAttributeValue<float>(Attribute.AreaOfEffect);
 
         [CSharpCallLua]
         protected delegate void SpecialAbilityDelegate(string name);
         protected SpecialAbilityDelegate LuaSpecialAbility;
 
         public virtual void SpecialAbility() => LuaSpecialAbility?.Invoke(Name);
-        
+
         /// <summary>
         /// Loads the base stats of the hero from a Lua table.
         /// </summary>
@@ -28,13 +29,15 @@ namespace Player
         public void LoadBaseStats(LuaTable stats)
         {
             Name = stats.Get<string>("name");
-            GetOrCreateAttribute(Attribute.Health, stats.Get<float>("health"));
-            GetOrCreateAttribute(Attribute.Stamina, stats.Get<float>("stamina"));
-            GetOrCreateAttribute(Attribute.Speed, stats.Get<float>("speed"));
-            GetOrCreateAttribute(Attribute.AttackSpeed, stats.Get<float>("attack_speed"));
-            GetOrCreateAttribute(Attribute.AttackDamage, stats.Get<float>("attack_damage"));
-            GetOrCreateAttribute(Attribute.Armor, stats.Get<float>("armor"));
-            GetOrCreateAttribute(Attribute.AreaOfEffect, stats.Get<float>("aoe"));
+            this.GetOrCreateAttribute(Attribute.Health, stats.Get<float>("health"));
+            this.GetOrCreateAttribute(Attribute.Stamina, stats.Get<float>("stamina"));
+            this.GetOrCreateAttribute(Attribute.Speed, stats.Get<float>("speed"));
+            this.GetOrCreateAttribute(Attribute.AttackSpeed, stats.Get<float>("attack_speed"));
+            this.GetOrCreateAttribute(Attribute.AttackDamage, stats.Get<float>("attack_damage"));
+            this.GetOrCreateAttribute(Attribute.Armor, stats.Get<float>("armor"));
+            this.GetOrCreateAttribute(Attribute.AreaOfEffect, stats.Get<float>("aoe"));
         }
+
+        public Dictionary<Attribute, object> Attributes { get; } = new();
     }
 }
--- Assets/Scripts/Systems/Attributes/IAttributable.cs
+++ Assets/Scripts/Systems/Attributes/IAttributable.cs
@@ -12,30 +12,57 @@ namespace Systems.Attributes
         /// Dictionary to hold attributes with different types.
         /// </summary>
         Dictionary<Attribute, object> Attributes { get; }
+    }
+
+    /// <summary>
+    /// Methods for the IAttributable interface.
+    /// </summary>
+    public static class AttributableMethods
+    {
+        /// <summary>
+        /// Gets the value of an attribute.
+        /// </summary>
+        /// <typeparam name="T">The type of the attribute.</typeparam>
+        /// <param name="obj">The attributable object.</param>
+        /// <param name="attribute">The attribute type.</param>
+        /// <returns>The calculated value of the attribute.</returns>
+        public static T GetAttributeValue<T>(this IAttributable obj, Attribute attribute)
+            where T : struct, IComparable, IConvertible, IFormattable
+        {
+            return obj.Attributes.TryGetValue(attribute, out var instance) ? ((AttributeInstance<T>)instance).Value : default;
+        }
 
         /// <summary>
         /// Checks if the object has an attribute.
         /// </summary>
+        /// <param name="obj">The attributable object.</param>
         /// <param name="attribute">The attribute type.</param>
         /// <returns>True if the attribute is on the object.</returns>
-        bool HasAttribute(Attribute attribute);
+        public static bool HasAttribute(this IAttributable obj, Attribute attribute)
+        {
+            return obj.Attributes.ContainsKey(attribute);
+        }
 
         /// <summary>
         /// Gets or creates an attribute instance.
         /// </summary>
         /// <typeparam name="T">The type of the attribute.</typeparam>
+        /// <param name="obj">The attributable object.</param>
         /// <param name="attribute">The attribute type.</param>
         /// <param name="defaultValue">The default value if the attribute doesn't exist.</param>
         /// <returns>The attribute instance.</returns>
-        AttributeInstance<T> GetOrCreateAttribute<T>(Attribute attribute, T defaultValue)
-            where T : struct, IComparable, IConvertible, IFormattable;
+        public static AttributeInstance<T> GetOrCreateAttribute<T>(this IAttributable obj, Attribute attribute, T defaultValue)
+            where T : struct, IComparable, IConvertible, IFormattable
+        {
+            if (obj.Attributes.TryGetValue(attribute, out var instance))
+            {
+                return instance as AttributeInstance<T>;
+            }
 
-        /// <summary>
-        /// Gets the value of an attribute.
-        /// </summary>
-        /// <typeparam name="T">The type of the attribute.</typeparam>
-        /// <param name="attribute">The attribute type.</param>
-        /// <returns>The calculated value of the attribute.</returns>
-        T GetAttributeValue<T>(Attribute attribute) where T : struct, IComparable, IConvertible, IFormattable;
+            var newInstance = new AttributeInstance<T> { BaseValue = defaultValue };
+            obj.Attributes[attribute] = newInstance;
+
+            return newInstance;
+        }
     }
 }

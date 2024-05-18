0aae12b17501b0ba2d5463df5dc8fd721f0c33be
KingRainbow44 <kobedo11@gmail.com>
2024-05-18 19:44:46 -0400

    feat(terrain): Add button in script to generate world at runtime

---
+++ Assets/Scripts/Editor/WorldInspector.cs
@@ -0,0 +1,21 @@
+﻿using Runtime.World;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    [CustomEditor(typeof(World))]
+    public class WorldInspector : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            if (GUILayout.Button("Generate World"))
+            {
+                var world = target as World;
+                world!.Initialize();
+            }
+        }
+    }
+}

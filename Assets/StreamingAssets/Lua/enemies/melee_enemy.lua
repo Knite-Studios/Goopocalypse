3a95c36243653de85d405b378792c35644613477
KingRainbow44 <kobedo11@gmail.com>
2024-05-24 14:32:49 -0400

    misc(melee_enemy.lua): Change debug text & Nerf speed
    i needed the speed to be lower so i could actually hit the thing

--- Assets/StreamingAssets/Lua/enemies/melee_enemy.lua
+++ Assets/StreamingAssets/Lua/enemies/melee_enemy.lua
@@ -1,13 +1,12 @@
 -- These are loaded when the instance is created.
 base_stats = {
-    health = 20,
     max_health = 20,
-    speed = 9.8,
+    speed = 4.2,
     armor = 1,
 }
 
 -- Event handler for when a player casts a special ability.
 -- context: The 'Hero' object instance which cast the ability.
 function On_SpecialAbility(context)
-    print("Archer " .. context.Name .. " used a special ability.")
+    print("Melee enemy " .. context.Name .. " used a special ability.")
 end

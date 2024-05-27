995aea875e58a201d5b2ccbd3ff5ca00865a2fd1
KingRainbow44 <kobedo11@gmail.com>
2024-05-26 20:50:49 -0400

    feat(network): Use a custom network manager
    Co-authored-by: Joaquin <joaquinzh17@gmail.com>

---
+++ Assets/Scripts/Runtime/Managers/NetworkManager.cs
@@ -0,0 +1,19 @@
+﻿using Mirror;
+using UnityEngine;
+
+namespace Managers
+{
+    public class NetworkManager : Mirror.NetworkManager
+    {
+        public override void OnServerConnect(NetworkConnectionToClient conn)
+        {
+            LobbyManager.OnPlayerConnected?.Invoke(conn);
+        }
+
+        public override void OnServerDisconnect(NetworkConnectionToClient conn)
+        {
+            base.OnServerDisconnect(conn);
+            LobbyManager.OnPlayerDisconnected?.Invoke(conn);
+        }
+    }
+}

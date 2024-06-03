2437166f6f7b984c871bae5a2370cfd42a46dd76
KingRainbow44 <kobedo11@gmail.com>
2024-06-03 14:00:00 +0100

    feat(ui): Load all resources into global constants
    Co-authored-by: Joaquin <joaquinzh17@gmail.com>

---
+++ UserInterface/src/ui/resources.ts
@@ -0,0 +1,4 @@
+export const ThaleahFat = resource.loadFont(`${__dirname}/resources/ThaleahFat.ttf`);
+
+export const Logo = resource.loadImage(`${__dirname}/resources/logo.png`);
+export const Placeholder = resource.loadImage(`${__dirname}/resources/placeholder.png`);

cc1aa30298b80dd566fd205edc8efcf67c73faea
KingRainbow44 <kobedo11@gmail.com>
2024-06-06 15:07:04 -0400

    feat(ui): Change prev. menu to debug
    this also contains scaffolding for the final menu screen
    
    Co-authored-by: Joaquin <joaquinzh17@gmail.com>

--- UserInterface/src/ui/screens/MenuScreen.tsx
+++ UserInterface/src/ui/screens/MenuScreen.tsx
@@ -1,73 +1,25 @@
 import { h } from "preact";
-import { useState } from "preact/hooks";
-
-import Button from "@components/Button";
-import Text, { Size } from "@components/Text";
 
 import type { ScriptManager } from "game";
 
 function MenuScreen({ game }: { game: ScriptManager }) {
-    const [address, setAddress] = useState("127.0.0.1");
-    const [port, setPort] = useState(7777);
-
     return (
-        <div
-            style={{ width: "50%" }}
-            class={"p-5 text-white"}
-        >
-            <Text size={Size.Normal} class={"mb-4"}>Hello World!</Text>
+        <div>
+            <div>
 
-            <Button
-                class={"mb-4 bg-blue-500 "}
-                onClick={() => {
-                    game.GameManager.StartDebugServer();
-                    game.GameManager.StartGame();
-                }}
-            >
-                Start Game (host + server)
-            </Button>
-
-            <div class={"flex-row w-full mb-4"}>
-                <textfield
-                    text={"127.0.0.1"}
-                    class={"text-black w-1/2 text-2xl"}
-                    onInput={(e) => setAddress(e.newData)}
-                />
-
-                <textfield
-                    text={"7777"}
-                    class={"text-black w-1/2 text-2xl"}
-                    onInput={(e) => setPort(parseInt(e.newData))}
-                />
             </div>
 
-            <Button
-                class={"mb-4 bg-blue-500"}
-                onClick={() => {
-                    game.GameManager.JoinDebugServer(address, port);
-                    game.GameManager.StartGame();
-                }}
-            >
-                Join Game (client)
-            </Button>
+            <div>
+                <div>
+                    <button>
+                        Credits
+                    </button>
 
-            <Button
-                class={"mb-4 bg-blue-500"}
-                onClick={() => {
-                    game.LobbyManager.MakeLobby();
-                }}
-            >
-                Create Steam Lobby
-            </Button>
-
-            <Button
-                class={"mb-4 bg-blue-500"}
-                onClick={() => {
-                    game.GameManager.StartGame();
-                }}
-            >
-                Start Game
-            </Button>
+                    <button>
+                        Select
+                    </button>
+                </div>
+            </div>
         </div>
     );
 }

bb2fb79abfc656e59f8f4a68e35cdc3043737eb5
Joaquin <joaquinzh17@gmail.com>
2024-06-09 21:58:48 -0600

    feat(enemy): Refine Random Enemy behavior

--- Assets/Scripts/Runtime/Entity/Enemies/RandomEnemy.cs
+++ Assets/Scripts/Runtime/Entity/Enemies/RandomEnemy.cs
@@ -7,8 +7,11 @@ namespace Entity.Enemies
     public class RandomEnemy : Enemy
     {
         [TitleHeader("Random Enemy Settings")]
-        [SerializeField] private Vector2 randomPoint = new Vector2(-5.0f, 5.0f);
+        [SerializeField] private float patrolRadius = 5.0f;
         [SerializeField] private float detectionDistance = 5.0f;
+        [SerializeField] private float patrolCooldown = 2.0f;
+
+        private bool _isPatrolling = true;
 
         protected override IEnumerator UpdatePath()
         {
@@ -17,17 +20,39 @@ namespace Entity.Enemies
                 if (Target && Vector2.Distance(transform.position, Target.position) <= detectionDistance)
                 {
                     CurrentPath = Pathfinder.FindPath(Target.position);
+                    _isPatrolling = false;
                 }
                 else
                 {
-                    var randomPosition = new Vector2(Random.Range(randomPoint.x, randomPoint.y),
-                        Random.Range(randomPoint.x, randomPoint.y));
-                    CurrentPath = Pathfinder.FindPath(randomPosition);
+                    if (!_isPatrolling)
+                    {
+                        _isPatrolling = true;
+                    }
+                    else
+                    {
+                        var randomPosition = GetRandomWalkablePositionAround(transform.position, patrolRadius);
+                        if (randomPosition.HasValue) CurrentPath = Pathfinder.FindPath(randomPosition.Value);
+                    }
+
+                    yield return new WaitForSeconds(patrolCooldown);
                 }
 
                 CurrentPathIndex = 0;
                 yield return new WaitForSeconds(0.3f);
             }
+        }
+
+        /// <summary>
+        /// Gets a random walkable position around the specified position within a given radius.
+        /// </summary>
+        private Vector2? GetRandomWalkablePositionAround(Vector2 center, float radius)
+        {
+            // NOTE: Might be best to do recurssion here.
+            var grid = Pathfinder.grid;
+            var randomPosition = center + Random.insideUnitCircle * radius;
+            var node = grid!.GetNode(randomPosition);
+
+            return node != null && node.isWalkable ? randomPosition : null;
         }
     }
 }

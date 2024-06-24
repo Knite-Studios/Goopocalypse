0d90552e4c7776959c673152483d210191db2351
Percy <perzeus.ttv@gmail.com>
2024-06-24 14:36:12 -0600

    feat: Wave Manager
    Over time and projectile fixed

--- Assets/Scripts/Runtime/Managers/WaveManager.cs
+++ Assets/Scripts/Runtime/Managers/WaveManager.cs
@@ -14,6 +14,10 @@ namespace Managers
         [Tooltip("The amount of seconds it takes to spawn a wave.")]
         public int spawnThreshold = 10;
 
+        [SerializeField]
+        [Tooltip("Enable to spawn waves over time; disable to spawn waves manually.")]
+        private bool spawnOverTime = true;
+
         private bool _gameRunning;
 
         protected override void OnAwake()
@@ -28,12 +32,8 @@ namespace Managers
 
         #region Packet Handlers
 
-        /// <summary>
-        /// Invoked when the server notifies the clients of the current wave info.
-        /// </summary>
         private static void OnWaveInfo(WaveInfoS2CNotify notify)
         {
-            // Do not run if we are the host.
             if (NetworkServer.activeHost) return;
 
             Instance.WaveCount = notify.wave;
@@ -42,54 +42,51 @@ namespace Managers
 
         #endregion
 
-        /// <summary>
-        /// Invoked when the game starts.
-        /// </summary>
         private void OnGameStart()
         {
-            // Do not run if we are a client.
             if (!NetworkServer.activeHost) return;
 
             _gameRunning = true;
             WaveCount = 1;
 
-            StartCoroutine(CountTimer());
+            if (spawnOverTime)
+            {
+                StartCoroutine(CountTimer());
+            }
         }
 
-        /// <summary>
-        /// Called once per second.
-        /// </summary>
         private void Tick()
         {
-            if (_matchTimer % spawnThreshold == 0)
+            if (_matchTimer % spawnThreshold == 0 && spawnOverTime)
             {
                 SpawnWave();
                 WaveCount++;
             }
 
-            // Broadcast wave info.
-            NetworkServer.SendToAll(new WaveInfoS2CNotify
-                { wave = WaveCount, timer = MatchTimer });
+            NetworkServer.SendToAll(new WaveInfoS2CNotify { wave = WaveCount, timer = MatchTimer });
         }
 
-        /// <summary>
-        /// Counts the time elapsed since the game started.
-        /// </summary>
         private IEnumerator CountTimer()
         {
             while (_gameRunning)
             {
                 yield return new WaitForSeconds(1);
                 MatchTimer++;
-
                 Tick();
             }
         }
 
-        /// <summary>
-        /// Creates a wave of enemies.
-        /// TODO: Add a correct entity count.
-        /// </summary>
         public void SpawnWave() => GameManager.OnWaveSpawn?.Invoke(_waveCount);
+
+        // Manual wave spawn, can be called from a UI button when spawnOverTime is false
+        public void ManualSpawnWave()
+        {
+            if (!spawnOverTime)
+            {
+                SpawnWave();
+                WaveCount++;
+                MatchTimer = 0; // Reset timer if spawning manually
+            }
+        }
     }
 }

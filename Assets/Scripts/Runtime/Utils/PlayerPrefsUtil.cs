82bf33b0738264911c9cd3c3ee66283c17bcd647
Joaquin <joaquinzh17@gmail.com>
2024-06-29 19:27:27 -0600

    feat(AudioManager): Implement audio callbacks

---
+++ Assets/Scripts/Runtime/Utils/PlayerPrefsUtil.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Utils
+{
+    public static class PlayerPrefsUtil
+    {
+        public static float MusicVolume
+        {
+            get => PlayerPrefs.GetFloat("MusicVolume", 1.0f);
+            set
+            {
+                PlayerPrefs.SetFloat("MusicVolume", value);
+                PlayerPrefs.Save();
+            }
+        }
+
+        public static float SoundFxVolume
+        {
+            get => PlayerPrefs.GetFloat("SoundFxVolume", 1.0f);
+            set
+            {
+                PlayerPrefs.SetFloat("SoundFxVolume", value);
+                PlayerPrefs.Save();
+            }
+        }
+    }
+}

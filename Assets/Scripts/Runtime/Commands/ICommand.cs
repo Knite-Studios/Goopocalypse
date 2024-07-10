eaa374ea7aac7eb51134d72fc015fdfa6650e51e
Joaquin <joaquinzh17@gmail.com>
2024-07-09 20:38:40 -0600

    feat(Commands): Implement Command Pattern

---
+++ Assets/Scripts/Runtime/Commands/ICommand.cs
@@ -0,0 +1,7 @@
+﻿namespace Commands
+{
+    public interface ICommand
+    {
+        void Execute();
+    }
+}
---
+++ Assets/Scripts/Runtime/Managers/CommandManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Commands;
+using UnityEngine.SceneManagement;
+
+namespace Managers
+{
+    public class CommandManager : MonoSingleton<CommandManager>
+    {
+        private readonly Queue<ICommand> _commands = new();
+
+        private void Update()
+        {
+            if (_commands.Count > 0)
+                _commands.Dequeue().Execute();
+        }
+
+        protected override void OnSceneUnloaded(Scene scene)
+            => _commands.Clear();
+
+        public void AddCommand(ICommand command)
+            => _commands.Enqueue(command);
+    }
+}
